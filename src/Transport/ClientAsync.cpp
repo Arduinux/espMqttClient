bf5aa2210492c13af6ed4ac2b3638b6c712635ef
Arduinux <89458473+Arduinux@users.noreply.github.com>
2026-01-09 23:20:27 +0100

    Client capable of using Ethernet interfaces
    * "WiFi" replaced by "Network".
    * Example with MQTT-PSK and Ethernet interface added.
    * Removes warning: 'void AsyncClient::close(bool)' is deprecated: Use AsyncClient::close() instead [-Wdeprecated-declarations].
    * Warning in ClientAsync.cpp: Change limited to ESP32 and AsyncTCP versions >= 3.4.10.

--- src/Transport/ClientAsync.cpp
+++ src/Transport/ClientAsync.cpp
@@ -42,7 +42,13 @@ int ClientAsync::read(uint8_t* buf, size_t size) {
 }
 
 void ClientAsync::stop() {
+#if defined(ARDUINO_ARCH_ESP32)
+# if ASYNCTCP_VERSION_NUM >= ((3 << 16) | (4 << 8) | 10)
+  client.close();
+# else
   client.close(false);
+# endif  // ASYNCTCP_VERSION_NUM >= ((3 << 16) | (4 << 8) | 10)
+#endif  // defined(ARDUINO_ARCH_ESP32)
 }
 
 bool ClientAsync::connected() {
